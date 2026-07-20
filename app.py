41cdd2c05ea87f1316cac119f988423e68cc5720
Nyxkoenner <Peter.Feser@googlemail.com>
2026-07-20 18:33:03 +0200

    neue Version nach Split

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-"""Streamlit-Einstiegspunkt für den modularen Aktien Explorer."""
+"""Streamlit-Einstiegspunkt für den modularen Aktien Explorer.."""
 from stock_explorer.legacy_app import main
 
 if __name__ == "__main__":
